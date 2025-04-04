7066c806bfa60d88d805172581006be5de7d2870
Gilson <gilson.elias@live.com>
2025-04-04 16:22:27 -0300

    seller dao

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,31 @@
+package model.dao.impl;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao {
+    @Override
+    public void insert(Seller obj) {
+
+    }
+
+    @Override
+    public void update(Seller obj) {
+
+    }
+
+    @Override
+    public void deleteByid(Integer id) {
+
+    }
+
+    @Override
+    public Seller findById(integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return null;
+    }
+}
