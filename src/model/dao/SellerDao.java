9e0f321a66787ffaf901af972a6651b8fc070329
Gilson <gilson.elias@live.com>
2025-04-04 21:44:40 -0300

    findByDepartment

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -1,5 +1,6 @@
 package model.dao;
 
+import model.entities.Department;
 import model.entities.Seller;
 import java.util.List;
 
@@ -10,6 +11,7 @@ public interface SellerDao {
     void deleteByid(Integer id);
     Seller findById(Integer id);
     List<Seller> findAll();
+    List<Seller> findByDepartment(Department department);
 
 
 }
